b9102c759168a2d1d3b0942bd24e2e8cb13118fc
Bilal <bhaidar@gmail.com>
2023-06-07 10:07:11 +0300

    Add tests to test column reordering with one column without cards

--- tests/Feature/Http/Controllers/CardsReorderUpdateControllerTest.php
+++ tests/Feature/Http/Controllers/CardsReorderUpdateControllerTest.php
@@ -5,18 +5,16 @@ use App\Models\Card;
 use App\Models\Column;
 use App\Models\User;
 
-use Inertia\Testing\Assert;
-
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\actingAs;
 
 beforeEach(function () {
     $this->user = User::factory()->create([
-        'email' => 'bhaidar@gmail.com',
+        'email' => 'test@gmail.com',
     ]);
     
-    $this->board = Board::factory()->create();
+    $this->board = Board::factory()->for($this->user)->create();
     $this->columns = Column::factory()->for($this->board)->count(2)->create();
     
     $this->columns->each(function ($column) {
@@ -60,5 +58,41 @@ it ('reorders cards inside columns successfully', function () {
                   ->has('cards', 2)
                   ->etc()
               )
+        );
+});
+
+it ('reorders cards inside columns successfully by removing all cards from last column', function () {
+
+    // Prepare payload
+    $payload[] = [
+        'id' => $this->board->columns()->first()->id,
+        'cards' => $this->board->columns->map(function ($column) {
+            return $column->cards->map(function ($card) {
+                return [
+                    'id' => $card->id,
+                    'position' => $card->position,
+                ];
+            });
+        })->flatten(1)
+            ->toArray(),
+    ];
+
+    // Visit the board page
+    actingAs($this->user)
+        ->get(route('boards', ['board' => $this->board->id]));
+
+    // Do a reorder
+    actingAs($this->user)
+        ->followingRedirects()
+        ->put(route('cards.reorder'), ['columns' => $payload])
+        ->assertOk()
+        ->assertInertia(
+            fn(AssertableInertia $page) => $page
+                ->component('Kanban')
+                ->has('board.data', 3)
+                ->has('board.data.columns.0', fn (AssertableInertia $assert) => $assert
+                    ->has('cards', 4)
+                    ->etc()
+                )
         );
 })->only();
